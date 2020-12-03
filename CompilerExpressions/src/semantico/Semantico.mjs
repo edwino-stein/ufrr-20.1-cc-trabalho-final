b321775d37ae003ad7982d089370b21bd24845a1
Edwino Stein <edwino.stein@gmail.com>
2020-12-03 18:36:24 -0400

    Adicionado classe Semantico
    Classe responsável por realizar a análise semantica da linguagem
    
    Issue #20

---
+++ CompilerExpressions/src/semantico/Semantico.mjs
@@ -0,0 +1,8 @@
+import SimboloIdentificador from './SimboloIdentificador.mjs';
+import ErroSemantico from '../exception/ErroSemantico.mjs';
+
+export default class Semantico {
+    constructor(arvore) {
+        this._arvore = arvore;
+    }
+}
