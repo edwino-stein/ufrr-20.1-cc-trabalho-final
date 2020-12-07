f8b49e44b88b69dfc4d35e872573efa9c8c9dd46
Edwino Stein <edwino.stein@gmail.com>
2020-12-07 01:55:37 -0400

    Definido a cor primária da aplicação
    Issue #46

--- aplicacao/src/plugins/vuetify.js
+++ aplicacao/src/plugins/vuetify.js
@@ -8,4 +8,11 @@ export default new Vuetify({
     icons: {
         iconfont: 'mdi', // default - only for display purposes
     },
+    theme: {
+        themes: {
+            light: {
+                primary: '#52beb3'
+            },
+        },
+    }
 });
