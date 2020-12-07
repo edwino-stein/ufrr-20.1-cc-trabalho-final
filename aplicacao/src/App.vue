37be87f02ea6356644d622321c6ebc24ba9ba01e
Edwino Stein <edwino.stein@gmail.com>
2020-12-07 01:52:27 -0400

    Adicionado componente ListaDeArquivos
    Issue #46

--- aplicacao/src/App.vue
+++ aplicacao/src/App.vue
@@ -1,24 +1,78 @@
 <template>
-    <v-app>
-        <v-main>
-            <HelloWorld />
-        </v-main>
+    <v-app class="primary">
+        <v-container class="pa-0" >
+            <v-row no-gutters >
+                <v-col cols="3" class="pa-3">
+                    <v-card flat out color="primary">
+                        <v-toolbar flat dark dense short color="primary">
+                            <v-toolbar-title>
+                                <strong>CompilerExpressions</strong>
+                            </v-toolbar-title>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                icon
+                                small
+                                light
+                                color="primary"
+                                class="white"
+                                elevation="1"
+                            > <v-icon>mdi-plus</v-icon> </v-btn>
+                        </v-toolbar>
+                        <ListaDeArquivos />
+                    </v-card>
+                    <v-footer padless color="primary white--text text-center">
+                        <p style="width: 100%" class="mb-0">
+                            <strong>Edwino A. L. Stein</strong> — 2020.1
+                        </p>
+                    </v-footer>
+                </v-col>
+                <v-col cols="9">
+                    <v-container class="ml-0 pl-0">
+                        <v-main>
+                        </v-main>
+                    </v-container>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import ListaDeArquivos from './components/ListaDeArquivos';
 
 export default {
     name: 'App',
 
     components: {
-        HelloWorld
+        ListaDeArquivos,
     },
 
     data: () => ({}),
 };
 </script>
+
+<style>
+
+    .v-application {
+        background: #52beb3 !important;
+    }
+
+    .v-application--wrap > .container {
+        max-width: 1585px;
+    }
+
+    .v-application--wrap > .container,
+    .v-application--wrap > .container > .row,
+    .v-application--wrap > .container > .row > .col > .container,
+    .v-application--wrap > .container > .row > .col > .container  > .v-main {
+        height: 100%;
+    }
+
+    .v-application--wrap > .container > .row > .col > .v-card {
+        height: calc(100% - 20px);
+    }
+
+</style>
 
 <style lang="sass">
     @import '../node_modules/typeface-roboto/index.css'
---
+++ aplicacao/src/components/ListaDeArquivos.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-list dense shaped dark color="primary">
+        <v-list-item-group>
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-icon>mdi-file</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>Arquivo 1</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-icon>mdi-file</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>Arquivo 2</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-icon>mdi-file</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>Arquivo 3</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list-item-group>
+    </v-list>
+</template>
+
+<script>
+    export default {
+        name: 'ListaDeArquivos',
+        data: () => ({}),
+    }
+</script>
