4538f00188c72cc0a6066f46c2241d805ec92dda
Edwino Stein <edwino.stein@gmail.com>
2020-12-08 17:50:42 -0400

    Adicionado index.mjs principal do módulo CompilerExpressions
    Issue #55

---
+++ CompilerExpressions/index.mjs
@@ -0,0 +1,11 @@
+import Sintatico from './src/sintatico/Sintatico.mjs';
+import Semantico from './src/semantico/Semantico.mjs';
+import Intermediario from './src/sintese/Intermediario.mjs';
+import Mips from './src/sintese/Mips.mjs';
+
+export {
+    Sintatico,
+    Semantico,
+    Intermediario,
+    Mips
+};
